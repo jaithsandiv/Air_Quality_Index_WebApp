755937d107d32436b184bc34dd442efbeb2c9cba
jaithsandiv <jaithsandivhemachandra@gmail.com>
2025-03-20 16:04:31 +0530

    Add project files.

---
+++ Air_Quality_Index_WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Air_Quality_Index_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
